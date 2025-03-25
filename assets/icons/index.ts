74bb5777e4fbdd145fc4af60e23f13877d9a0d75
Akashpatel077 <127598617+Akashpatel077@users.noreply.github.com>
2025-03-25 15:50:49 +0530

    Initial Commit

---
+++ assets/icons/index.ts
@@ -0,0 +1,8 @@
+import Buy from './Buy.svg';
+import Heart from './Heart.svg';
+import Home from './Home.svg';
+import Notification from './Notification.svg';
+import Paper from './Paper.svg';
+import Profile from './Profile.svg';
+
+export {Buy, Heart, Home, Notification, Paper, Profile};
